b5091dde1e870d94264573615f7e34306292a6a4
NickRiley7 <nick_riley@LAPTOP-J8V35VGC>
2023-11-11 01:14:13 -0500

    navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css'
+import Red from './components/Red'
+import Blue from './components/Blue'
+import Home from './components/Home'
+
+function App() {
+
+
+  return (
+    <>
+      
+
+      <div id="container">
+      
+      <div id="navbar">
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+        <Link to="/home">home</Link>
+      </div>
+      
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue/>} />
+          <Route path="/red" element={<Red/>} />
+          <Route path="/home" element={<Home/>} />
+        </Routes>
+      </div>
+    
+      </div>
+    </>
+  )
+}
+
+export default App
